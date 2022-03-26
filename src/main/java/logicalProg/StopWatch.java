e285b763b0c6f86654bfed254e551f68de51a25c
unknown <mohit16gaikwad@gmail.com>
2022-03-26 14:20:36 +0530

    StopWatch a

--- src/main/java/logicalProg/StopWatch.java
+++ src/main/java/logicalProg/StopWatch.java
@@ -2,22 +2,20 @@ package logicalProg;
 import java.util.Scanner;
 
 public class StopWatch {
-	/*
-	 * static variables for storing start and end time
-	 */
+	
 	static double start = 0, stop = 0;
 
-	/**
-	 * Function to put start time and print it
-	 */
+	
+	 // Function to put start time and print it
+	 
 	static void startTime() {
 		start = System.currentTimeMillis();
 		System.out.println("started at " + start);
 	}
 
-	/**
-	 * function to put stop time and print it
-	 */
+	
+//	 *function to put stop time and print it*
+	
 	static void stopTime() {
 		stop = System.currentTimeMillis();
 		System.out.println("stopped at " + stop);
@@ -27,7 +25,7 @@ public class StopWatch {
 	 * function to calculate elapsed time and print it
 	 */
 	static void elapsedTime() {
-		System.out.println("elapsed time is " + (stop - start) / 1000);
+		System.out.println("elapsed time is IN SECONDS " + (stop - start) / 1000);
 	}
 
 	public static void main(String[] args) {
